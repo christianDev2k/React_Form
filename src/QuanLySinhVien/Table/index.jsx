117cd0036754fa0cecb9e8105714bf29db468bf2
Hoang Nhan <hnhan793@gmail.com>
2023-08-31 00:07:20 +0700

    fix bug

--- src/QuanLySinhVien/Table/index.jsx
+++ src/QuanLySinhVien/Table/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { ButtonStyled } from '../GlobalStyles';
 import { TableContext } from '../../context/TableContext';
 import { useSelector } from 'react-redux';
